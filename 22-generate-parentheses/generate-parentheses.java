f6bd80e48d6d53d481177c8b07b8ba65e15a6aee
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-03-28 07:13:42 +0530

    Time: 2 ms (52.29%) | Memory: 43.1 MB (49.24%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -5,17 +5,15 @@ class Solution {
         return result;    
     }
     public void func(int n, List<String> result, String ans, int open, int close){
-        if(open == n && close == n){
+        if(ans.length() == 2*n){
             result.add(ans);
             return;
         }
-        if(open > n){
-            return;
+        if(open < n){
+            func(n, result, ans + "(", open + 1, close);
         }
-        if(close > open){
-            return;
+        if(close < open){
+            func(n, result, ans + ")", open, close + 1);
         }
-        func(n, result, ans + "(", open + 1, close);
-        func(n, result, ans + ")", open, close + 1);
     }
 }
