783ca35bfbc5f8585f7b67c3cae19537d9af419e
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-03-28 12:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (72.49%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n != 1 && n != 4){
+            n = sum(n);
+        }
+        return n ==1;
+    }
+    public int sum(int n){
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum += (digit*digit);
+            n /= 10;
+        }
+        return sum;
+    }
+}
