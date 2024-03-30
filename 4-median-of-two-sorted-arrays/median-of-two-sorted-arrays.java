cf8a3fc556501eab380d749684043a3a2c1f926d
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-03-30 21:52:00 +0530

    Time: 2 ms (64.70%) | Memory: 46.1 MB (37.81%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,44 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr = merge_array(nums1, nums2);
-        Arrays.sort(arr);
-        return (double)median(arr);
-        
-    }
-    public static int[] merge_array(int[] arr1, int[] arr2){
-        int n = arr1.length;
-		int m = arr2.length;
-		int[] ans = new int[n + m];
-		int i = 0, j = 0, k = 0;
-		while (i < n && j < m) {
-			if (arr1[i] < arr2[j]) {
-				ans[k] = arr1[i];
-				i++;
-				k++;
-			} else {
-				ans[k] = arr2[j];
-				j++;
-				k++;
-			}
-		}
-		while (i < n) {
-			ans[k] = arr1[i];
-			i++;
-			k++;
-		}
-		while (j < m) {
-			ans[k] = arr2[j];
-			j++;
-			k++;
-		}
-		return ans;
-    }
-    public static float median(int[] arr){
-        int n = arr.length;
-        if(n%2 != 0){
-            return (float)arr[n/2];
+        int[] ans = merge(nums1, nums2);
+        int n = ans.length;
+        if(n % 2 != 0){
+            return (double)ans[n/2];
         }
-        return ((float)arr[n/2] + (float)arr[(n-1)/2])/2;
-        
+        double a1 = ans[n/2];
+        double a2 = ans[n/2 - 1];
+        return (a1+a2)/2;
     }
+    public static int[] merge(int[] arr1, int[] arr2){
+        int i=0; 
+        int j=0;
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int[] ans = new int[n1 + n2];
+        int k = 0;
+        while(i < n1 & j < n2){
+            if(arr1[i] <= arr2[j]){
+                ans[k++] = arr1[i++];
+            }
+            else{
+                ans[k++] = arr2[j++];
+            }
+        }
+        while(i < n1){
+            ans[k++] = arr1[i++];
+        }
+        while(j < n2){
+            ans[k++] = arr2[j++];
+        }
+        return ans;
+    } 
 }
