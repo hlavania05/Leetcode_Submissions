59b8fdc9b6f1231769f2656f8c9029924dabd246
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-06-19 07:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (27.82%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,12 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i=1; i<n; i+=3){
-            if(nums[i] != nums[i-1]){
-                return nums[i-1];
-            }
+        int ones = 0;
+        int twos = 0;
+        for(int i=0; i<nums.length; i++){
+            ones = (ones ^ nums[i]) & (~twos);
+            twos = (twos ^ nums[i]) & (~ones);
         }
-        return nums[n-1];
+        return ones;
     }
 }
