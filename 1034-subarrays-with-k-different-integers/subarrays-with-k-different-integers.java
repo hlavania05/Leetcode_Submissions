3922779342a66fbfed2d6eb77933626a281b5c2e
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-07-14 23:04:05 +0530

    Time: 38 ms (83.37%) | Memory: 47.8 MB (12.54%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -3,6 +3,9 @@ class Solution {
         return cntsubarrays(nums, k) - cntsubarrays(nums, k-1);
     }
     public int cntsubarrays(int[] arr, int k){
+        if(k<0){
+            return 0;
+        }
         int l = 0;
         int r = 0;
         int cnt = 0;
@@ -11,11 +14,10 @@ class Solution {
             int x = arr[r];
             map.put(x, map.getOrDefault(x, 0)+1);
             while(map.size() > k){
-                int left = arr[l];
-                map.put(left, map.get(left)-1);
-                if(map.get(left) == 0){
-                    map.remove(left);
+                if(map.get(arr[l]) == 1){
+                    map.remove(arr[l]);
                 }
+                else { map.put(arr[l], map.get(arr[l])-1); }
                 l++;
             }
             cnt += (r-l+1);
