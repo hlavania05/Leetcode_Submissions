56e409ab47e189e5bd46aeaea5d2bae7ae0691e1
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-08-20 10:41:09 +0530

    Time: 4 ms (100.00%) | Memory: 44 MB (33.07%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,22 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        return Palindrome(x);
-    }
-    public static Boolean Palindrome(int n){
-        int rev = reverse(n);
-        if(rev == n){
-            return true;
+        if(x < 0 || (x%10 == 0 && x != 0))
+        {
+            return false;
+        } 
+        int revNum = 0;
+        while(x > revNum){
+            revNum = revNum * 10 + (x % 10);
+            x /= 10;
         }
-        return false;
-    }
-    public static int reverse(int num){
-        int rev = 0;
-        while(num>0){
-            int rem = num%10;
-            rev = rev*10 + rem;
-            num = num/10;
-        }
-        return rev;
-
+        return x == revNum || x == revNum/10;
     }
 }
