19623ffad308dd5a2ca1a4d787afeb37bc10d922
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-09-18 11:44:07 +0530

    Time: 3 ms (55.52%) | Memory: 43.8 MB (5.58%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,29 +1,18 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0, j = 0;
-        List<Integer> arr = new ArrayList<>();
-        while(i < nums1.length && j <nums2.length){
-            if(nums1[i] == nums2[j]){
-                if(!arr.contains(nums1[i])){
-                    arr.add(nums1[i]);
-                }
-                i++;
-                j++;
-            }
-            else if(nums1[i] < nums2[j]){
-                i++;
-            }
-            else{
-                j++;
-            }
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int i : nums1){
+            set1.add(i);
         }
-        int[] ans = new int[arr.size()];
-        int x = 0;
-        for(int num : arr){
-            ans[x] = num;
-            x++;
+        for(int j : nums2){
+            set2.add(j);
+        }
+        set1.retainAll(set2);
+        int[] ans = new int[set1.size()];
+        int idx = 0;
+        for(int item : set1){
+            ans[idx++] = item;
         }
         return ans;
 
