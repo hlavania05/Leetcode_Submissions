fece1095d9499274b4f240df729d789bf5486c9a
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-09-21 10:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (93.66%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -1,37 +1,32 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
-        return func(preorder, new int[]{0}, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        return func(preorder, Integer.MAX_VALUE, new int[]{0}, preorder.length);
     }
-
-    public TreeNode func(int[] preorder, int[] idx, int min, int max) {
-        if (idx[0] >= preorder.length) {
+    public TreeNode func(int[] preorder, int upperBound, int[] i, int n){
+        if(i[0] >= n || preorder[i[0]] > upperBound){
             return null;
         }
+        TreeNode root = new TreeNode(preorder[i[0]++]);
 
-        int key = preorder[idx[0]];
-        TreeNode root = null;
-
-        if (key > min && key < max) {
-            root = new TreeNode(key);
-            idx[0]++;
-            root.left = func(preorder, idx, min, key);
-            root.right = func(preorder, idx, key, max);
-        }
+        root.left = func(preorder, root.val, i, n);
+        root.right = func(preorder, upperBound, i, n);
         return root;
     }
+
+    
 }
