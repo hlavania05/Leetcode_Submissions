db77ff68744b52bec299d74389857fdde4757f1b
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-09-25 17:00:30 +0530

    Time: 47 ms (72.84%) | Memory: 47.3 MB (67.05%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -2,27 +2,27 @@ class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
         return cntsubarrays(nums, k) - cntsubarrays(nums, k-1);
     }
+    // counting subarrays whose have distinct elements less than or equal to k;
     public int cntsubarrays(int[] arr, int k){
-        if(k<0){
-            return 0;
-        }
         int l = 0;
         int r = 0;
         int cnt = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         while(r < arr.length){
-            int x = arr[r];
-            map.put(x, map.getOrDefault(x, 0)+1);
+            int item = arr[r];
+            map.put(item, map.getOrDefault(item, 0)+1);
             while(map.size() > k){
-                if(map.get(arr[l]) == 1){
-                    map.remove(arr[l]);
+                int left = arr[l];
+                map.put(left, map.get(left)-1);
+                if(map.get(left) == 0){
+                    map.remove(left);
                 }
-                else { map.put(arr[l], map.get(arr[l])-1); }
                 l++;
             }
             cnt += (r-l+1);
             r++;
         }
         return cnt;
+        
     }
 }
