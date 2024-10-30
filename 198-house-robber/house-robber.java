468a0bee51a57d1d35287ced00f6515f49381a22
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-10-30 23:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (88.10%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
         int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-        return func(nums, 0, dp);
-    }
-    public static int func(int[] house, int idx, int[] dp){
-        if(idx >= house.length){
-            return 0;
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<nums.length; i++){
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
         }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int choose = house[idx] + func(house, idx+2, dp);
-        int not_choose = func(house, idx+1, dp);
-
-        return dp[idx] = Math.max(choose, not_choose);
+        return dp[nums.length-1];
     }
 }
