81d444fd1c09b24e0994ae488f211e8e4bc28c5c
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2024-12-31 01:54:50 +0530

    Time: 148 ms (48.18%) | Memory: 162.1 MB (55.95%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,26 +1,46 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
-        }
-        
-        Set<Integer> visited = new HashSet<>();
-        return dfs(source, destination, graph, visited);
+        return graph(edges, source, destination, n);
     }
-    
-    private boolean dfs(int node, int destination, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
-        if (node == destination) {
-            return true;
+    public boolean graph(int[][] edges, int source, int destination, int n){
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int i=0; i<edges.length; i++) {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            if(map.containsKey(a)){
+                map.get(a).add(b);
+            }
+            else{
+                List<Integer> list = new ArrayList<>();
+                list.add(b);
+                map.put(a, list);
+            }
+            if(map.containsKey(b)){
+                map.get(b).add(a);
+            }
+            else{
+                List<Integer> list = new ArrayList<>();
+                list.add(a);
+                map.put(b, list);
+            }
         }
-        visited.add(node);
-        for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
-            if (!visited.contains(neighbor)) {
-                if (dfs(neighbor, destination, graph, visited)) {
-                    return true;
+        return BFS(map, source, destination, n);
+    }
+    public boolean BFS(HashMap<Integer, List<Integer>> map, int src, int dest, int n){
+        int[] visited = new int[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(src);
+        visited[src] = 1;
+        while(!queue.isEmpty()){
+            int front = queue.poll();
+            if(front == dest){
+                return true;
+            }
+            List<Integer> lst = map.get(front);
+            for(int item : lst){
+                if(visited[item] == 0) {
+                    queue.offer(item);
+                    visited[item] = 1;
                 }
             }
         }
