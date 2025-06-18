bb5faa48148f8421451fa6796830372ed2f5081d
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-06-18 09:49:26 +0530

    Time: 42 ms (20.69%) | Memory: 44.5 MB (10.96%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,17 +1,16 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        List<Integer> lst = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
         for(int i=1; i<=n; i++){
-            lst.add(i);
+            q.offer(i);
+        } 
+
+        while(q.size() > 1){
+            for(int j=1; j<k; j++){
+                q.offer(q.poll());
+            }
+            q.poll();
         }
-        return func(lst, k, 0);
-    }
-    public int func(List<Integer> lst, int k, int i){
-        if(lst.size() == 1){
-            return lst.get(0);
-        }
-        int idx = (i+k-1) % (lst.size());
-        lst.remove(idx);
-        return func(lst, k, idx);
+        return q.peek();
     }
 }
