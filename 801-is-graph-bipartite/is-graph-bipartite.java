3fd628c7e819f71dbe4a15fa037878184b348886
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-08-24 08:34:32 +0530

    Time: 1 ms (61.58%) | Memory: 45.3 MB (28.57%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,43 +1,33 @@
-class BipartitePair{
-    int vtx;
-    int dis;
-    public BipartitePair(int vtx, int dis){
-        this.dis = dis;
-        this.vtx = vtx;
-    }
-}
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        Queue<BipartitePair> q = new LinkedList<>();
-        HashMap<Integer, Integer> visited = new HashMap<>();
-
-        for(int vtx=0; vtx<graph.length; vtx++){
-            if(visited.containsKey(vtx)){
-                continue;
+        int n = graph.length;
+        int[] visited = new int[n];
+        Arrays.fill(visited, -1);
+ 
+        for(int i=0; i<n; i++){
+            if(visited[i] == -1){
+                if(!bfs(i, graph, visited)){
+                    return false;
+                }
             }
-            // BFS
-            q.add(new BipartitePair(vtx, 0));
-            while(!q.isEmpty()){
-                //1. Remove
-                BipartitePair rp = q.poll();
+        }
+        return true;
+    }
+    public boolean bfs(int src, int[][] graph, int[] visited){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(src);
+        visited[src] = 0;
 
-                //2. Ignore
-                if(visited.containsKey(rp.vtx)){
-                    if(visited.get(rp.vtx) != rp.dis){
-                        return false;
-                    }
-                    continue;
+        while(queue.size() != 0){
+            int parent = queue.poll();
+            for(int neighbour : graph[parent]){
+                if(visited[neighbour] == -1){
+                    visited[neighbour] = 1 - visited[parent];
+                    queue.offer(neighbour);
                 }
-                //3. Add visited
-                visited.put(rp.vtx, rp.dis);
-
-                //5. Add university nbrs
-                for(int nbrs: graph[rp.vtx]){
-                    if(!visited.containsKey(nbrs)){
-                        q.add(new BipartitePair(nbrs, rp.dis+1));
-                    }
+                else if(visited[neighbour] == visited[parent]){
+                    return false;
                 }
-
             }
         }
         return true;
