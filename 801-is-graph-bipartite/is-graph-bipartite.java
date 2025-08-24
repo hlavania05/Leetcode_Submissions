db570518478a236b92e48d8139f1a442a68fbf2f
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-08-24 08:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (91.23%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -6,31 +6,51 @@ class Solution {
  
         for(int i=0; i<n; i++){
             if(visited[i] == -1){
-                if(!bfs(i, graph, visited)){
+                if(!dfs(i, -1, graph, visited)){
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean bfs(int src, int[][] graph, int[] visited){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(src);
-        visited[src] = 0;
+    // public boolean bfs(int src, int[][] graph, int[] visited){
+    //     Queue<Integer> queue = new LinkedList<>();
+    //     queue.offer(src);
+    //     visited[src] = 0;
 
-        while(queue.size() != 0){
-            int parent = queue.poll();
-            for(int neighbour : graph[parent]){
-                if(visited[neighbour] == -1){
-                    visited[neighbour] = 1 - visited[parent];
-                    queue.offer(neighbour);
-                }
-                else if(visited[neighbour] == visited[parent]){
+    //     while(queue.size() != 0){
+    //         int parent = queue.poll();
+    //         for(int neighbour : graph[parent]){
+    //             if(visited[neighbour] == -1){
+    //                 visited[neighbour] = 1 - visited[parent];
+    //                 queue.offer(neighbour);
+    //             }
+    //             else if(visited[neighbour] == visited[parent]){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    public boolean dfs(int src, int parent, int[][] graph, int[] visited){
+        if(parent == -1){
+            visited[src] = 0;
+        }
+        else{
+            visited[src] = 1-visited[parent];
+        }
+        for(int item : graph[src]){
+            if(visited[item] == -1){
+                if(!dfs(item, src, graph, visited)){
                     return false;
                 }
             }
+            if(visited[item] == visited[src]){
+                return false;
+            }
         }
         return true;
+
     }
 
 }
