fa3802d14b26911d2c6768f56520541f77eb00db
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-09-17 18:45:56 +0530

    Time: 55 ms (12.68%) | Memory: 55.3 MB (40.66%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,23 +1,25 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+        // maximum length of substring with 2 distinct integer
+        int left = 0;
+        int right = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        int len = 0;
-        int i = 0;
-        int j = 0;
-        while(j < fruits.length){
-            int item = fruits[j];
-            map.put(item, map.getOrDefault(item, 0) + 1);
+        int maxLen = Integer.MIN_VALUE;
+
+        while(right < fruits.length){
+            int item = fruits[right];
+            map.put(item, map.getOrDefault(item, 0)+1);
             while(map.size() > 2){
-                int left = fruits[i];
-                map.put(left, map.get(left)-1);
-                if(map.get(left) == 0){
-                    map.remove(left);
+                int leftItem = fruits[left];
+                map.put(leftItem, map.getOrDefault(leftItem, 0)-1);
+                if(map.get(leftItem) == 0){
+                    map.remove(leftItem);
                 }
-                i++;
+                left++;
             }
-            len = Math.max(len, j-i+1);
-            j++;
+            maxLen = Math.max(maxLen, right-left+1);
+            right++;
         }
-        return len;
+        return maxLen;
     }
 }
