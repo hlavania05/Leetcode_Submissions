6994870d35c998b86a3dac84774da62376e6f004
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-09-18 10:42:02 +0530

    Time: 29 ms (22.92%) | Memory: 45.2 MB (25.76%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,29 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length+1][amount+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        int ans = func(coins, amount, dp, coins.length-1);
-        return ans >= (int)1e9 ? -1 : ans;
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for (int[] row : dp) Arrays.fill(row, -1);
+
+        int ans = helper(n - 1, amount, coins, dp);
+        return (ans >= (int)1e9) ? -1 : ans; // agar answer infinity hua toh -1
     }
-    public int func(int[] arr, int amount, int[][] dp, int idx){
-        if(idx == 0){
-            if(amount % arr[idx] == 0){
-                return amount / arr[idx];
-            }
-            return (int)1e9;
+
+    private int helper(int idx, int target, int[] coins, int[][] dp) {
+        // base case
+        if (idx == 0) {
+            if (target % coins[0] == 0) return target / coins[0];
+            else return (int)1e9; // impossible
         }
 
-        if(dp[idx][amount] != -1){
-            return dp[idx][amount];
-        }
-        int notTake = func(arr, amount, dp, idx-1);
+        if (dp[idx][target] != -1) return dp[idx][target];
+
+        // notTake
+        int notTake = helper(idx - 1, target, coins, dp);
+
+        // take
         int take = (int)1e9;
-        if(arr[idx] <= amount){
-            take = 1 + func(arr, amount-arr[idx], dp, idx);
+        if (coins[idx] <= target) {
+            take = 1 + helper(idx, target - coins[idx], coins, dp);
         }
 
-        return dp[idx][amount] = Math.min(take, notTake);
+        return dp[idx][target] = Math.min(take, notTake);
     }
 }
