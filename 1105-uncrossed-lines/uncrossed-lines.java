8b0c2ca6664bec7e0bff649e1be193479a243b82
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-09-18 17:21:54 +0530

    Time: 6 ms (69.08%) | Memory: 44.3 MB (43.12%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.java
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -1,27 +1,25 @@
 class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int[][] dp = new int[nums1.length][nums2.length];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
-        return helper(nums1, nums2, dp, 0, 0);
+        return func(nums1, nums2, 0, 0, dp);
     }
-    public int helper(int[] arr1, int[] arr2, int[][] dp, int i, int j){
+    public int func(int[] arr1, int[] arr2, int i, int j, int[][] dp){
         if(i == arr1.length || j == arr2.length){
             return 0;
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        int ans = 0;
         if(arr1[i] == arr2[j]){
-            ans = 1 + helper(arr1, arr2, dp, i+1, j+1);
+            return dp[i][j] = 1 + func(arr1, arr2, i+1, j+1, dp);
         }
-        else{
-            int f = helper(arr1, arr2, dp, i+1, j);
-            int s = helper(arr1, arr2, dp, i, j+1);
-            ans = Math.max(f,s);
+        else {
+            int a = func(arr1, arr2, i+1, j, dp);
+            int b = func(arr1, arr2, i, j+1, dp);
+            return dp[i][j] = Math.max(a,b);
         }
-        return dp[i][j] = ans;
     }
 }
