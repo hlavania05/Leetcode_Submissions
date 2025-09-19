5fe0e9dce6e4eab0e7e336949387909793d77481
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-09-19 08:33:06 +0530

    Time: 93 ms (21.97%) | Memory: 57.5 MB (25.11%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,36 +1,35 @@
 class Solution {
-    public int arrSum(int[] arr){
+    public boolean canPartition(int[] nums) {
+        int totalSum = sum(nums);
+        if(totalSum % 2 != 0){
+            return false;
+        }
+        int target = totalSum/2;
+        Boolean[][] dp = new Boolean[nums.length][target+1];
+        return helper(nums, 0, target, dp); 
+    }
+    public boolean helper(int[] arr, int idx, int target, Boolean[][] dp){
+        if(target == 0){
+            return true;
+        }
+        if(idx == arr.length){
+            return false;
+        }
+        if(dp[idx][target] != null) {
+            return dp[idx][target];
+        }
+        boolean notTake = helper(arr, idx+1, target, dp);
+        boolean take = false;
+        if(arr[idx] <= target){
+            take = helper(arr, idx+1, target-arr[idx], dp);
+        }
+        return dp[idx][target] = notTake || take;
+    }
+    public static int sum(int[] arr){
         int sum = 0;
         for(int item : arr){
             sum += item;
         }
         return sum;
-    }
-    public boolean canPartition(int[] nums) {
-        int N = nums.length;
-        int totalSum = arrSum(nums);
-        if(totalSum % 2 == 1){
-            return false;
-        }
-        int sum = totalSum/2;
-        Boolean[][] dp = new Boolean[N+1][sum+1];
-        
-        return helper(dp, nums, sum, N-1);
-    }
-    public boolean helper(Boolean[][] dp, int[] arr, int sum, int n){
-        if(n < 0 || sum < 0){
-            return false;
-        }
-        if(sum == 0 || arr[n] == sum){
-            return true;
-        }
-        
-        if(dp[n][sum] != null){
-            return dp[n][sum];
-        }
-        boolean take = helper(dp, arr, sum - arr[n], n-1);
-        boolean notTake = helper(dp, arr, sum, n-1);
-        dp[n][sum] = notTake || take;
-        return dp[n][sum];
     }
 }
