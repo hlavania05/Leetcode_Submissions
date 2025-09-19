dfe09a658618e62473d4cdde2806aa5d054500ff
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-09-19 17:36:55 +0530

    Time: 3 ms (95.05%) | Memory: 45.4 MB (18.53%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,26 +1,29 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int[][] dp = new int[word1.length()][word2.length()];
-        return func(word1, word2, 0, 0, dp);
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(word1, word2, 0, 0, dp);
     }
-    public int func(String s1, String s2, int i, int j, int[][] dp){
-        if(i == s1.length()){
-            return s2.length()-j;
+    public int helper(String s, String t, int i, int j, int[][] dp){
+        if(i == s.length()){
+            return t.length()-j;
         }
-        if(j == s2.length()){
-            return s1.length()-i;
+        if(j == t.length()){
+            return s.length()-i;
         }
-        if(dp[i][j] != 0){
+        if(dp[i][j] != -1){
             return dp[i][j];
         }
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = func(s1, s2, i+1, j+1, dp);
+        if(s.charAt(i) == t.charAt(j)){
+            return dp[i][j] = helper(s, t, i+1, j+1, dp);
         }
         else{
-            int insert = func(s1, s2, i, j+1, dp);
-            int delete = func(s1, s2, i+1, j, dp);
-            int replace = func(s1, s2, i+1, j+1, dp);
-            return dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+            int insert = helper(s, t, i, j+1, dp);
+            int delete = helper(s, t, i+1, j, dp);
+            int update = helper(s, t, i+1, j+1, dp);
+            return dp[i][j] = 1 + Math.min(insert, Math.min(delete, update));
         }
     }
 }
