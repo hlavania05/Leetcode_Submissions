0c1ee570a1718d08b8dcee07bf346489eb7c518c
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-09-19 18:41:09 +0530

    Time: 25 ms (7.17%) | Memory: 43 MB (6.53%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,15 +3,26 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        int[] map1 = new int[200];
-        int[] map2 = new int[200];
-        for(int i=0; i<s.length(); i++){
-            if(map1[s.charAt(i)] != map2[t.charAt(i)]){
+        HashMap<Character, Character> st = new HashMap<>();
+        HashMap<Character, Character> ts = new HashMap<>();
+        for(int i=0; i<t.length(); i++){
+            char charT = s.charAt(i);
+            char charS = t.charAt(i);
+            if(st.containsKey(charS) && st.get(charS) != charT){
                 return false;
+            }  
+            else{
+                st.put(charS, charT);
             }
-            map1[s.charAt(i)] = i+1;
-            map2[t.charAt(i)] = i+1;
+
+            if(ts.containsKey(charT) && ts.get(charT) != charS){
+                return false;
+            }  
+            else{
+                ts.put(charT, charS);
+            }
         }
         return true;
+        
     }
 }
