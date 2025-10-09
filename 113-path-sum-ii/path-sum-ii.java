74596e6b5e87701460cc2b2605e5c52b8ee00c5b
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2025-10-09 11:10:07 +0530

    Time: 1 ms (99.83%) | Memory: 44.7 MB (37.53%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -17,23 +17,25 @@ class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> lst = new ArrayList<>();
-        func(root, targetSum, ans, lst);
+        helper(root, targetSum, ans, lst);
         return ans;
     }
-    public void func(TreeNode root, int targetSum, List<List<Integer>> ans, List<Integer> lst){
+    public void helper(TreeNode root, int targetSum, List<List<Integer>> ans, List<Integer> lst){
         if(root == null){
             return;
         }
         lst.add(root.val);
         if(root.left == null && root.right == null){
             if(targetSum - root.val == 0){
-                ans.add(new ArrayList(lst));
+                ans.add(new ArrayList<>(lst));
+
             }
         }
+        
         else{
-            func(root.left, targetSum - root.val, ans, lst);
-            func(root.right, targetSum - root.val, ans, lst);
+            helper(root.left, targetSum-root.val, ans, lst);
+            helper(root.right, targetSum-root.val, ans, lst);
         }
-        lst.remove(lst.size() - 1);
+        lst.remove(lst.size()-1);
     }
 }
