0b6393db54e132565abd97d959d1e8dc80314d25
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2026-03-13 21:58:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (48.40%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,20 @@
 class Solution {
     public int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
         int[] dp = new int[n+1];
-        return func(n,dp);
-        
+        dp[0] = 0;
+        dp[1] = 1;
+        return help(n, dp); 
     }
-    public int func(int n, int[] dp){
-        if(n == 1 || n==0){
+    public int help(int n, int[] dp){
+        if(n == 0 || n == 1){
             return n;
         }
         if(dp[n] != 0){
             return dp[n];
         }
-        return dp[n] = func(n-1, dp) + func(n-2, dp);
+        return dp[n] = help(n-1, dp) + help(n-2, dp);
     }
 }
