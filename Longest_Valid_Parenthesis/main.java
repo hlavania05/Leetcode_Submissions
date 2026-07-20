0285dc59a46140ea56c05675ef640ee13077eec9
hlavania05 <lavaniaharshita05@gmail.com>
2026-07-20 18:57:33 +0530

    Longest Valid Parenthesis Code in JAVA

---
+++ Longest_Valid_Parenthesis/main.java
@@ -0,0 +1,46 @@
+package String.Longest_Valid_Parenthesis;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class main {
+
+    public static int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // input
+        System.out.print("Enter the parentheses string: ");
+        String input = scanner.nextLine();
+
+        // calling function
+        int result = longestValidParentheses(input);
+
+        // result
+        System.out.println("Longest Valid Parentheses Length: " + result);
+        scanner.close();
+    }
+}
