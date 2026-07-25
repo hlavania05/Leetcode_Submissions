b38eb41288cd3d3f4f445d8498c2d8d6020a95b0
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2026-07-25 08:36:16 +0530

    Time: 2 ms (99.29%) | Memory: 44 MB (92.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,14 +1,41 @@
 class Solution {
     // brute force
+    // public boolean isPalindrome(String s) {
+    //     String str = "";
+    //     for(char ch : s.toCharArray()){
+    //         if(Character.isLetterOrDigit(ch)){
+    //             str += Character.toLowerCase(ch);
+    //         }
+    //     }
+    //     return checkPalindrome(str);
+    // }
+
+    // optimized - two pointers
     public boolean isPalindrome(String s) {
-        String str = "";
-        for(char ch : s.toCharArray()){
-            if(Character.isLetterOrDigit(ch)){
-                str += Character.toLowerCase(ch);
+        int i = 0;
+        int j = s.length()-1;
+        while(i < j){
+            int left = s.charAt(i);
+            int right = s.charAt(j);
+            if(Character.isLetterOrDigit(left)){
+                if(Character.isLetterOrDigit(right)){
+                    if(Character.toLowerCase(left) != Character.toLowerCase(right)){
+                        return false;
+                    }
+                    i++;
+                    j--;
+                }
+                else{
+                    j--;
+                }
+            }
+            else{
+                i++;
             }
         }
-        return checkPalindrome(str);
+        return true;
     }
+
     public boolean checkPalindrome(String s){
         int i = 0;
         int j = s.length()-1;
@@ -23,8 +50,5 @@ class Solution {
 
     }
 
-    // optimized - two pointers
-    // public boolean isPalindrome(String s) {
-        
-    // }
+    
 }
