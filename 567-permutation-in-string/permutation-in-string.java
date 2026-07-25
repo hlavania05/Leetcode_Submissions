f84e7afd627fe62cfca2ac97e10536485e68bdae
Harshita Lavania <143238630+hlavania05@users.noreply.github.com>
2026-07-25 18:15:45 +0530

    Time: 22 ms (29.88%) | Memory: 46.9 MB (15.33%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,42 +1,42 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s2.length() < s1.length()){
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if(n2 < n1){
             return false;
         }
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map1 = new HashMap<>();
         for(char ch : s1.toCharArray()){
-            map.put(ch,map.getOrDefault(ch, 0)+1);
+            map1.put(ch, map1.getOrDefault(ch, 0)+1);
         }
 
-        int l = 0;
-        int r = 0;
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        while(r < s1.length()){
-            char ch = s2.charAt(r);
-            map1.put(ch, map1.getOrDefault(ch, 0)+1);
-            r++;
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        int i=0;
+        int j=0;
+        while(j < n1){
+            char ch = s2.charAt(j);
+            map2.put(ch, map2.getOrDefault(ch, 0)+1);
+            j++;
         }
-        if(map.equals(map1)){
+        if(map1.equals(map2)){
             return true;
         }
-        while(r < s2.length()){
-            char left = s2.charAt(l);
-            map1.put(left, map1.getOrDefault(left, 0)-1);
-            if(map1.get(left) == 0){
-                map1.remove(left);
-            }
-            l++;
+        while(j < n2){
+            char left = s2.charAt(i);
+            map2.put(left, map2.getOrDefault(left, 0)-1);
+            if(map2.get(left) == 0) {
+                map2.remove(left);
+            } 
+            i++;
 
-            char right = s2.charAt(r);
-            map1.put(right, map1.getOrDefault(right, 0)+1);
-            r++;
+            char right = s2.charAt(j);
+            map2.put(right, map2.getOrDefault(right, 0)+1);
+            j++;
 
-            if(map.equals(map1)){
+            if(map1.equals(map2)){
                 return true;
             }
         }
         return false;
-
-
     }
 }
